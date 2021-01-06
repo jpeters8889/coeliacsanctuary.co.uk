4a78b87c276666316f992342ea331009dc2dd6d6
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-06 18:46:01 +0000

    shop mass discount functionality

---
+++ App/Modules/Shop/Architect/MassDiscountsBlueprint.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Coeliac\Modules\Shop\Architect;
+
+use Carbon\Carbon;
+use JPeters\Architect\Plans\Group;
+use JPeters\Architect\Plans\Boolean;
+use JPeters\Architect\Plans\DateTime;
+use JPeters\Architect\Plans\Textfield;
+use JPeters\Architect\Blueprints\Blueprint;
+use Coeliac\Modules\Shop\Models\ShopCategory;
+use Coeliac\Modules\Shop\Models\ShopMassDiscount;
+
+class MassDiscountsBlueprint extends Blueprint
+{
+    public function blueprintSite(): string
+    {
+        return 'Shop';
+    }
+
+    public function blueprintName(): string
+    {
+        return 'Mass Discounts';
+    }
+
+    public function blueprintRoute(): string
+    {
+        return 'shop-mass-discounts';
+    }
+
+    public function model(): string
+    {
+        return ShopMassDiscount::class;
+    }
+
+    public function plans(): array
+    {
+        return [
+            Textfield::generate('title'),
+            Textfield::generate('percentage'),
+            DateTime::generate('start_at')->setDefault(Carbon::now()->toIso8601String()),
+            DateTime::generate('end_at')->setDefault(Carbon::now()->addWeek()->toIso8601String()),
+            DateTime::generate('created_at')->hideOnForms(),
+
+            Group::generate('categories')
+                ->hideOnIndex()
+                ->setPivotRelationship('assignedCategories')
+                ->plans($this->getCategories()),
+        ];
+    }
+
+    public function getCategories(): array
+    {
+        return ShopCategory::query()
+            ->withLiveProducts()
+            ->orderBy('title')
+            ->get()
+            ->transform(static function (ShopCategory $category) {
+                return Boolean::generate($category->id, $category->title)->setDefault('0');
+            })
+            ->toArray();
+    }
+}
