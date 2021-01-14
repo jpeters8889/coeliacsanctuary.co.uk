eebc079d7265b07a55f3b1c5fb782731bf424880
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-14 18:37:07 +0000

    optimisations

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -17,7 +17,7 @@
             </div>
             <div class="flex flex-1 p-8">
                 <div class="flex-1 flex flex-col">
-                    <div class="flex flex-col justify-center items-center -mt-4 mb-4 space-y-2">
+                    <div class="flex flex-col justify-center items-center -mt-4 mb-4 space-y-2" style="min-height: 160px;">
                         <img class="w-1/2 max-h-logo" width="100%" height="125" src="/assets/svg/logo.svg" alt="Coeliac Sanctuary"/>
                         <h1 class="text-4xl font-medium font-coeliac mb-2 text-center">Coeliac Sanctuary</h1>
                     </div>
