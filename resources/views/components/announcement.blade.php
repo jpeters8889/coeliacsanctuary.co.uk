260825380b36f8f11bee5e835db26796972dafb1
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-15 19:35:26 +0000

    wip

--- resources/views/components/announcement.blade.php
+++ resources/views/components/announcement.blade.php
@@ -1,18 +1,19 @@
 @if($announcements->count() > 0)
-    <div class="bg-red-dark p-1 text-center text-white cursor-pointer" style="min-height: 60px;">
-        <announcement>
+    <announcement>
+        <div class="bg-red-dark p-1 text-center text-white">
             <div class="flex flex-col">
                 <div class="mb-2 font-semibold">
                     {{ $announcements->first()->title }}
                 </div>
-                <a class="cursor-pointer text-white-80 text-sm hover:text-white hover:underline transition-color">
+                <a class="cursor-pointer text-white-80 text-sm hover:text-white hover:underline transition-color"
+                   @click="showModal = true">
                     Read more
                 </a>
             </div>
+        </div>
 
-            <template v-slot:body>
-                {!! $announcements->first()->text !!}
-            </template>
-        </announcement>
-    </div>
+        <template v-slot:body>
+            {!! $announcements->first()->text !!}
+        </template>
+    </announcement>
 @endif
