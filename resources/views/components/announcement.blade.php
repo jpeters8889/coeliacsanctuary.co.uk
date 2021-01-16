e4cc27fd6aff2d161f44e3be64f72fb540b98dd4
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-16 09:54:03 +0000

    wip

--- resources/views/components/announcement.blade.php
+++ resources/views/components/announcement.blade.php
@@ -1,11 +1,9 @@
 @if($announcements->count() > 0)
-    <div class="bg-red-dark" style="min-height: 72px">
-        <announcement>
-            <template v-slot:title>
-                {{ $announcements->first()->title }}
-            </template>
+    <announcement>
+        <template v-slot:title>
+            {{ $announcements->first()->title }}
+        </template>
 
-            {!! $announcements->first()->text !!}
-        </announcement>
-    </div>
+        {!! $announcements->first()->text !!}
+    </announcement>
 @endif
