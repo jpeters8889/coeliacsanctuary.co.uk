19d3ac181dbde6c2001a18d50fda038101a1daa5
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-22 19:17:23 +0000

    article header

--- resources/js/Components/ArticleHeader.vue
+++ resources/js/Components/ArticleHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <h2>
-        <slot></slot>
-    </h2>
+        <h2 class="border-b border-blue-light font-semibold leading-none py-2 text-lg">
+            <slot></slot>
+        </h2>
 </template>
 
 <script>
