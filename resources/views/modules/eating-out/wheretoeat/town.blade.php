148db478216c20c08c09ad1e4e0ef669f8bdbba3
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-24 17:32:09 +0000

    town page

--- resources/views/modules/eating-out/wheretoeat/town.blade.php
+++ resources/views/modules/eating-out/wheretoeat/town.blade.php
@@ -1,16 +1,43 @@
 @extends('templates.page-two-column')
 
 @section('primary-column')
-    <div class="flex flex-col"chunk>
+    <div class="flex flex-col" chunk>
         <div class="page-box">
-            <h1 class="text-2xl font-coeliac text-center font-semibold leading-tight md:text-left">
-                Gluten Free Places to eat in {{ $town->town }}, {{ $county->county }}<br />
-                <a class="text-xs font-sans hover:text-grey transition-color" href="/wheretoeat/{{ $county->slug }}">Back
-                    to {{ $county->county }}</a>
+            <h1 class="my-4 p-3 text-3xl font-coeliac text-center font-semibold leading-tight border-b border-t border-blue-light">
+                Gluten Free Places to eat in {{ $town->town }}, {{ $county->county }}<br/>
             </h1>
 
+            <h6 class="text-center -mt-4 pt-1">
+                <a class="text-sm font-semibold font-sans hover:text-blue-dark transition-color"
+                   href="/wheretoeat/{{ $county->slug }}">
+                    Back to {{ $county->county }}
+                </a>
+            </h6>
+
             <div class="flex flex-col mt-2">
-                <div class="flex flex-col bg-blue-light-50 border text-sm border-blue p-2 mb-4">
+                <p class="mb-4">
+                    Here you can see all of the places in our Where to Eat guide that offer gluten free options in
+                    {{ $town->town }}, {{ $county->county }}, from cafes, restaurants, to attractions and hotels.
+                </p>
+
+                <p class="mt-2">
+                    Most of the places to eat listed in our guide are contributed by people like you, other Coeliac's or
+                    people with a gluten intolerance who know of local places in their local area and are kind enough to
+                    let us know.
+                </p>
+
+                <p class="mt-2">
+                    You won't find any nationwide chains in our normal eating out guide simply due to how many places
+                    these chains have, the other independent eateries will get lost! Instead, we list
+                    <a class="font-semibold hover:text-blue-dark transition-colour" href="/wheretoeat/nationwide">nationwide
+                        chains</a> on a separate page.
+                </p>
+
+                <p>
+                    You can use the tabs below to filter the results to find the type of places you want.
+                </p>
+
+                <div class="mt-2 flex flex-col bg-blue-light-50 border text-sm border-blue p-2">
                     <p>
                         Know somewhere that offers gluten free for us to add or does somewhere need removing off our
                         list? <a class="font-semibold hover:text-grey transition-color"
@@ -18,18 +45,9 @@
                     </p>
                 </div>
 
-                <wheretoeat-list :county-id="{{ $county->id }}" :town-id="{{ $town->id }}">
-                    <template v-slot:intro>
-                        <p class="mb-4">
-                            Here you can see all of the places in our Where to Eat guide that offer gluten free options in
-                            {{ $town->town }}, {{ $county->county }}.
-                        </p>
-                        <p>
-                            You can use the tabs above to filter the results to find the type of places you want.
-                        </p>
-{{--                        <google-ad code="5284484376"></google-ad>--}}
-                    </template>
-                </wheretoeat-list>
+                <div class="min-h-map">
+                    <wheretoeat-list :county-id="{{ $county->id }}" :town-id="{{ $town->id }}"></wheretoeat-list>
+                </div>
 
                 <google-ad code="5284484376"></google-ad>
 
