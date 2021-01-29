40f6cf6cfc9a989de4d14a8bbca5ef807244a00c
Jamie Peters <jamie@jamie-peters.co.uk>
2021-01-29 19:30:28 +0000

    search

---
+++ App/Modules/Search/Architect/Blueprint.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Coeliac\Modules\Search\Architect;
+
+use JPeters\Architect\Plans\DateTime;
+use JPeters\Architect\Plans\Textfield;
+use Coeliac\Modules\Search\Models\SearchHistory;
+use JPeters\Architect\Blueprints\Blueprint as Architect;
+
+class Blueprint extends Architect
+{
+    public function model(): string
+    {
+        return SearchHistory::class;
+    }
+
+    public function blueprintName(): string
+    {
+        return 'Search History';
+    }
+
+    public function blueprintRoute(): string
+    {
+        return 'search-history';
+    }
+
+    public function canEdit(): bool
+    {
+        return false;
+    }
+
+    public function ordering(): array
+    {
+        return ['number_of_searches', 'desc'];
+    }
+
+    public function plans(): array
+    {
+        return [
+            Textfield::generate('term'),
+
+            Textfield::generate('number_of_searches'),
+
+            DateTime::generate('created_at', 'First Search'),
+
+            DateTime::generate('updated_at', 'Most Recent Search'),
+        ];
+    }
+}
