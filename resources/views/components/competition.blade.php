096741c8e7b8d0e45997b8c05b0bb74f507a046d
Jamie Peters <jamie@jamie-peters.co.uk>
2021-03-17 19:34:34 +0000

    fix url

--- resources/views/components/competition.blade.php
+++ resources/views/components/competition.blade.php
@@ -1,6 +1,6 @@
 @if(isset($promoteCompetition) && is_object($promoteCompetition))
     <div class="inner-wrapper my-2">
-        <a class="block bg-yellow p-2 text-center text-white" href="/promoteCompetition/{{ $promoteCompetition->slug }}">
+        <a class="block bg-yellow p-2 text-center text-white" href="/competition/{{ $promoteCompetition->slug }}">
             <div class="flex flex-col">
                 <h3 class="mb-2 font-semibold">Enter our competition, {{ $promoteCompetition->name }}</h3>
                 <p>{{ $promoteCompetition->meta_description }}</p>
