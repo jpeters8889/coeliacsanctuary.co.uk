d33f8faffe2c0ce9e8f20e5219c1fce395c68613
Jamie Peters <jamie@jamie-peters.co.uk>
2021-11-19 18:01:09 +0000

    alter js bundles

--- resources/js/Components/Global/UI/index.js
+++ resources/js/Components/Global/UI/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
+import PopupCta from '~/Global/UI/PopupCta';
+import NewsletterSignup from '~/Global/UI/NewsletterSignup';
+import GoogleAd from '~/Global/UI/GoogleAd';
 
 const LinkButton = () => import('~/Global/UI/LinkButton' /* webpackChunkName: "chunk-link-button" */);
 const NumberCountUp = () => import('~/Global/UI/NumberCountUp' /* webpackChunkName: "chunk-number-count" */);
 const Accordion = () => import('~/Global/UI/Accordion' /* webpackChunkName: "chunk-accordion" */);
-const NewsletterSignup = () => import('~/Global/UI/NewsletterSignup' /* webpackChunkName: "chunk-newsletter-signup" */);
 const RecipeImage = () => import('~/Global/UI/RecipeImage' /* webpackChunkName: "chunk-recipe-image" */);
 const Stars = () => import('~/Global/UI/Stars' /* webpackChunkName: "chunk-stars" */);
-const PopupCta = () => import('~/Global/UI/PopupCta' /* webpackChunkName: "preload-popup-cta" */);
-const GoogleAd = () => import('~/Global/UI/GoogleAd' /* webpackChunkName: "preload-google-ad" */);
 
 Vue.component('GlobalUiAccordion', Accordion);
 Vue.component('GlobalUiGoogleAd', GoogleAd);
