bb402e77ba23d7ecc448ca15ac60d60f5a6c7286
Jamie Peters <jamie@jamie-peters.co.uk>
2022-02-14 18:05:30 +0000

    convert place recomendations to eateries

--- App/Modules/EatingOut/WhereToEat/Models/WhereToEatRecommendation.php
+++ App/Modules/EatingOut/WhereToEat/Models/WhereToEatRecommendation.php
@@ -7,6 +7,13 @@ namespace Coeliac\Modules\EatingOut\WhereToEat\Models;
 use Coeliac\Base\Models\BaseModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property string $place_name
+ * @property string $place_details
+ * @property string $place_location
+ * @property string $place_web_address
+ * @property number $place_venue_type_id
+ */
 class WhereToEatRecommendation extends BaseModel
 {
     protected $table = 'wheretoeat_place_recommendation';
