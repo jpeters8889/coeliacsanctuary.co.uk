3bdeedb28fa1edf809316847fa68fd019c14b9cf
Jamie Peters <jamie@jamie-peters.co.uk>
2022-02-19 16:15:24 +0000

    track transactional mail thorugh mailcoach

--- App/Common/Notifications/Messages/MJMLMessage.php
+++ App/Common/Notifications/Messages/MJMLMessage.php
@@ -16,6 +16,8 @@ class MJMLMessage extends MailMessage
 
     public function mjml(string $view, array|EmailData $data = []): static
     {
+        $this->store()->trackOpensAndClicks();
+
         $this->mjml = $this->view = $view;
 
         $this->markdown = null;
