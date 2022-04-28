962ed1b168d668f8a5fed31fc67c2f5a8efa66f7
Jamie Peters <jamie@jamie-peters.co.uk>
2022-04-28 18:44:18 +0100

    change links

--- App/Modules/EatingOut/WhereToEat/Support/LatestRatings.php
+++ App/Modules/EatingOut/WhereToEat/Support/LatestRatings.php
@@ -2,11 +2,31 @@
 
 namespace Coeliac\Modules\EatingOut\WhereToEat\Support;
 
+use Coeliac\Modules\EatingOut\WhereToEat\Models\WhereToEat;
 use Coeliac\Modules\EatingOut\WhereToEat\Models\WhereToEatReview;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 
 class LatestRatings
 {
+    protected function eateryLink(WhereToEat $eatery): string
+    {
+        if (Str::lower($eatery->town->town) === 'nationwide') {
+            return "/wheretoeat/nationwide/{$eatery->slug}";
+        }
+
+        return "/wheretoeat/{$eatery->county->slug}/{$eatery->town->slug}/{$eatery->slug}";
+    }
+
+    protected function townLink(WhereToEat $eatery): string
+    {
+        if (Str::lower($eatery->town->town) === 'nationwide') {
+            return "/wheretoeat/nationwide";
+        }
+
+        return "/wheretoeat/{$eatery->county->slug}/{$eatery->town->slug}";
+    }
+
     public function list(): Collection
     {
         return WhereToEatReview::query()
@@ -14,11 +34,11 @@ class LatestRatings
             ->latest()
             ->take(5)
             ->get()
-            ->transform(fn (WhereToEatReview $rating) => [
+            ->transform(fn(WhereToEatReview $rating) => [
                 'id' => $rating->id,
                 'eatery_id' => $rating->wheretoeat_id,
-                'slug' => "/wheretoeat/{$rating->eatery->county->slug}/{$rating->eatery->town->slug}/{$rating->eatery->slug}",
-                'town' => "/wheretoeat/{$rating->eatery->county->slug}/{$rating->eatery->town->slug}",
+                'slug' => $this->eateryLink($rating->eatery),
+                'town' => $this->townLink($rating->eatery),
                 'name' => $rating->eatery->name,
                 'location' => $rating->eatery->full_name,
                 'full_location' => $rating->eatery->full_location,
