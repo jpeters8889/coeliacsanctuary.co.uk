0b1c82443fb5d34a764363a6b69b3320b351c14d
Jamie Peters <jamie@jamie-peters.co.uk>
2022-09-01 19:26:18 +0100

    add new chart

---
+++ App/Modules/Shop/Architect/ProductSalesChart.php
@@ -0,0 +1,137 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Coeliac\Modules\Shop\Architect;
+
+use Carbon\Carbon;
+use Coeliac\Modules\Shop\Models\ShopOrderItem;
+use Coeliac\Modules\Shop\Models\ShopProduct;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Collection;
+use JPeters\Architect\Dashboards\Cards\Chartable;
+
+class ProductSalesChart extends Chartable
+{
+    protected array $colours = [
+        1 => '#80CCFC', //cards
+        2 => '#addaf9', // multi
+        3 => '#f26522', // cals
+        4 => '#ff0000', // wristbands
+        5 => '#00ff00', // stickers
+        6 => '#FFB6C1', // pens
+        7 => '#f5f5f5', // bags
+        8 => '#ecd14a', // coeliac other cards
+        9 => '#cccccc', // lanyard
+        10 => '#186ba3', // keyrings
+        11 => '#ecd14a', // coeliac plus
+    ];
+
+    protected array $backgroundColours = [];
+
+    protected function chartType(): string
+    {
+        return 'bar';
+    }
+
+    protected function products(): Collection
+    {
+        return ShopProduct::query()
+            ->with('categories')
+            ->orderBy('title')
+            ->get()
+            ->map(fn (ShopProduct $product) => ['id' => $product->id, 'title' => $product->title, 'category_id' => $product->categories[0]->id]);
+    }
+
+    protected function labels(): array
+    {
+        $labels = [];
+
+        $start = $this->dateRange['start']->clone()->startOf($this->dateRange['unit']);
+        $end = $this->dateRange['end']->clone()->endOf($this->dateRange['unit']);
+
+        $this->products()->each(function (array $product) use (&$labels, $start, $end) {
+            $count = ShopOrderItem::query()
+                ->where('product_id', $product['id'])
+                ->whereHas('order', fn (Builder $query) => $query->whereHas('payment'))
+                ->where('created_at', '>=', $start)
+                ->where('created_at', '<=', $end)
+                ->count();
+
+            if ($count) {
+                $labels[] = $product['title'];
+            }
+        });
+
+        return $labels;
+    }
+
+    protected function calculateData(): array
+    {
+        $start = $this->dateRange['start']->clone()->startOf($this->dateRange['unit']);
+        $end = $this->dateRange['end']->clone()->endOf($this->dateRange['unit']);
+
+        return $this->getData(
+            $start,
+            $end
+        );
+    }
+
+    protected function getData(Carbon $start, Carbon $end): int|float|array
+    {
+        $products = [];
+
+        $this->products()->each(function (array $product) use (&$products, $start, $end) {
+            $count = ShopOrderItem::query()
+                ->where('product_id', $product['id'])
+                ->whereHas('order', fn (Builder $query) => $query->whereHas('payment'))
+                ->where('created_at', '>=', $start)
+                ->where('created_at', '<=', $end)
+                ->count();
+
+            if ($count) {
+                $products[] = $count;
+                $this->backgroundColours[] = $this->colours[$product['category_id']];
+            }
+        });
+
+        return $products;
+    }
+
+    protected function data(): array
+    {
+        return [[
+            'label' => $this->dataLabel(),
+            'data' => $this->calculateData(),
+            'borderColor' => $this->backgroundColours,
+            'backgroundColor' => $this->backgroundColours,
+        ]];
+    }
+
+    protected function dataLabel(): string
+    {
+        return 'Product';
+    }
+
+    protected function defaultRange(): string
+    {
+        return 'lastMonth';
+    }
+
+    protected function options(): array
+    {
+        return [
+            'indexAxis' => 'y',
+            'scales' => [
+                'x' => [
+                    'min' => 0,
+                ],
+                'y' => [
+                    'ticks' => [
+                        'autoSkip' => false,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
--- App/Modules/Shop/Architect/ShopDashboard.php
+++ App/Modules/Shop/Architect/ShopDashboard.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Coeliac\Modules\Shop\Architect;
 
-use Carbon\Carbon;
-use Coeliac\Modules\Shop\Models\ShopOrder;
-use Illuminate\Support\Collection;
 use JPeters\Architect\Dashboards\AbstractDashboard;
 use JPeters\Architect\Dashboards\Cards\Chart;
 
@@ -27,36 +24,7 @@ class ShopDashboard extends AbstractDashboard
         return [
             Chart::make('Baskets', new BasketChart()),
             Chart::make('Income', new IncomeChart()),
+            Chart::make('products', new ProductSalesChart()),
         ];
-    }
-
-    protected function basketChart(): Chart
-    {
-        $chart = Chart::generate('Baskets');
-
-        $days = new Collection();
-
-        for ($x = 14; $x >= 0; --$x) {
-            $days->push(Carbon::today()->subDays($x));
-        }
-
-        $chart->addLabels($days->map(fn ($day) => $day->format('Y/m/d'))->toArray());
-
-        $baskets = [];
-        $converted = [];
-
-        foreach ($days as $day) {
-            $total = ShopOrder::query()
-                ->whereDate('created_at', '>=', $day->startOfDay())
-                ->whereDate('created_at', '<=', $day->endOfDay())
-                ->get();
-
-            $baskets[] = $total->count();
-            $converted[] = $total->whereNotNull('order_key')->count();
-        }
-        $chart->addDataSet('Total Baskets', $baskets, 'line');
-        $chart->addDataSet('Orders', $converted, 'line');
-
-        return $chart;
     }
 }
