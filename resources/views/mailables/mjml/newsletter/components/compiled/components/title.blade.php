2b96fcc9a035c2c53870ba6459dae5adce23a4a7
Jamie Peters <jamie@jamie-peters.co.uk>
2023-03-25 18:23:18 +0000

    style tweak

--- resources/views/mailables/mjml/newsletter/components/compiled/components/title.blade.php
+++ resources/views/mailables/mjml/newsletter/components/compiled/components/title.blade.php
@@ -1,7 +1,7 @@
 <mj-column>
-    <mj-text mj-class="inner" padding-bottom="10px">
+    <mj-text mj-class="inner" padding-bottom="10px" css-class="blue-links">
         <h2>
-            @if($link)<a href="{{ trim($link)}}" css-class="blue-links"> @endif
+            @if($link)<a href="{{ trim($link)}}"> @endif
                     {{ $title }}
             @if($link) </a>@endif
         </h2>
