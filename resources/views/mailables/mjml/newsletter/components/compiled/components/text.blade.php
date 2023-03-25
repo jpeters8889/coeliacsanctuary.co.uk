9a5767f2a00aefef96423bbdc03d1181ae8b5c51
Jamie Peters <jamie@jamie-peters.co.uk>
2023-03-25 18:04:49 +0000

    allow html in text block

--- resources/views/mailables/mjml/newsletter/components/compiled/components/text.blade.php
+++ resources/views/mailables/mjml/newsletter/components/compiled/components/text.blade.php
@@ -1,5 +1,5 @@
 <mj-column>
     @foreach($content as $line)
-        <mj-text mj-class="inner">{{ $line }}</mj-text>
+        <mj-text mj-class="inner">{!! $line !!}</mj-text>
     @endforeach
 </mj-column>
